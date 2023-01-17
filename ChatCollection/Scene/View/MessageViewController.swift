b22558662b2b4c16dc69e50451402f2395ce7d1a
wnsxor1993 <https://wnsxor1993@github.com>
2023-01-17 14:57:17 +0900

    refactor: LayoutIfNeeded 사용으로 수정

--- ChatCollection/Scene/View/MessageViewController.swift
+++ ChatCollection/Scene/View/MessageViewController.swift
@@ -12,9 +12,6 @@ import Then
 class MessageViewController: UIViewController {
 
     private lazy var messageCollectionView: UICollectionView = .init(frame: .zero, collectionViewLayout: .init()).then {
-//        let flowLayout = MessageCollectionViewLayout()
-//        flowLayout.delegate = self
-        
         guard let compositionalLayout = MessageCompositionalLayout().compositionalLayoutSection else {
             return }
         
@@ -34,14 +31,6 @@ class MessageViewController: UIViewController {
         self.view.backgroundColor = UIColor(red: 216/255, green: 238/255, blue: 192/255, alpha: 1)
         self.configureLayouts()
     }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        
-        guard !(self.messageCollectionView.visibleCells.isEmpty) else { return }
-        
-        self.messageCollectionView.collectionViewLayout.invalidateLayout()
-    }
 }
 
 extension MessageViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -54,7 +43,7 @@ extension MessageViewController: UICollectionViewDelegate, UICollectionViewDataS
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MessageCell.identifier, for: indexPath) as? MessageCell else { return .init() }
         
         cell.setCell(with: messages[indexPath.row].message, type: messages[indexPath.row].chatType)
-//        cell.layoutIfNeeded()
+        cell.layoutIfNeeded()
         
         return cell
     }
@@ -71,18 +60,3 @@ private extension MessageViewController {
         }
     }
 }
-
-//extension MessageViewController: MessageCollectionViewLayoutDelegate {
-//
-//    func collectionView(_ collectionView: UICollectionView, heightForCellAtIndexPath indexPath: IndexPath) -> CGFloat {
-//        let width = collectionView.bounds.width
-//        let estimateHeight: CGFloat = 200.0
-//        let dummyCell: MessageCell = .init(frame: .init(x: 0, y: 0, width: width, height: estimateHeight))
-//        dummyCell.setCell(with: messages[indexPath.row].message, type: messages[indexPath.row].chatType)
-//        dummyCell.layoutIfNeeded()
-//
-//        let estimateSize = dummyCell.systemLayoutSizeFitting(.init(width: width, height: estimateHeight))
-//
-//        return estimateSize.height
-//    }
-//}
